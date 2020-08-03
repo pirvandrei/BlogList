5eb42ccb0b2d2b2ed66fa5024a77ab9ffd1cc9d9
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-08-04 00:38:36 +0200

    refactiring tests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -31,4 +31,25 @@ blogRouter.post('/', async (request, response) => {
   response.json(savedBlog);
 });
 
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    likes: body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+  
+  response.json(updatedBlog);
+});
+
+blogRouter.delete('/:id', async (request, response) => {
+  const blog = await Blog.findByIdAndRemove(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end;
+  }
+});
+
 module.exports = blogRouter;
--- tests/blog-api.test.js
+++ tests/blog-api.test.js
@@ -13,68 +13,116 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
-});
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Luminite pe balcon',
-    author: 'Andrei Pirvan',
-    url: 'https://complementor.dk/',
-    likes: 7,
-  };
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs');
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-
-  const titles = blogsAtEnd.map((b) => b.title);
-  expect(titles).toContain('Luminite pe balcon');
-});
-
-test('default 0 when likes missing from request', async () => {
-  const newBlog = {
-    title: 'Luminite pe balcon',
-    author: 'Andrei Pirvan',
-    url: 'https://complementor.dk/',
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-
-  const likes = blogsAtEnd.map((b) => b.likes);
-  console.log(likes);
-  likes.forEach(like => {
-    expect(like).toBeGreaterThanOrEqual(0);
+    const titles = response.body.map((r) => r.title);
+    expect(titles).toContain('React patterns');
   });
 });
 
-test('blog without url and title is not added', async () => {
-  const newBlog = {
-    author: 'Andrei Pirvan',
-  };
+describe('addition of a blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Luminite pe balcon',
+      author: 'Andrei Pirvan',
+      url: 'https://complementor.dk/',
+      likes: 7,
+    };
 
-  await api.post('/api/blogs').send(newBlog).expect(404);
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
 
-  const response = await api.get('/api/blogs');
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+    const titles = blogsAtEnd.map((b) => b.title);
+    expect(titles).toContain('Luminite pe balcon');
+  });
+
+  test('default 0 when likes missing from request', async () => {
+    const newBlog = {
+      title: 'Luminite pe balcon',
+      author: 'Andrei Pirvan',
+      url: 'https://complementor.dk/',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const likes = blogsAtEnd.map((b) => b.likes);
+    likes.forEach((like) => {
+      expect(like).toBeGreaterThanOrEqual(0);
+    });
+  });
+
+  test('blog without url and title is not added', async () => {
+    const newBlog = {
+      author: 'Andrei Pirvan',
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(404);
+
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(200);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((r) => r.title);
+    expect(titles).not.toContain(blogToDelete.titles);
+  });
+});
+
+describe('updating of a blog', () => {
+  test('succeeds with status code 200 when likes is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToBeUpdated = blogsAtStart[0];
+
+    const newBlog = blogToBeUpdated;
+    newBlog.likes += 1;
+
+    await api.put(`/api/blogs/${blogToBeUpdated.id}`).send(newBlog).expect(200);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+});
 
 afterAll(() => {
   mongoose.connection.close();
